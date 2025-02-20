21d17c926441531c40ba3fab4a4e418bd3f98a52
psz3@wavus.co.kr <psz3@wavus.co.kr>
2025-02-20 17:10:00 +0900

    API호출 bookmark

--- src/app/bookmarks/add/page.tsx
+++ src/app/bookmarks/add/page.tsx
@@ -1,8 +1,52 @@
+'use client'
 
-const page = () => (
-  <div>
-		  <h1>Bookmarks Form</h1>
-  </div>
-);
+import React, {useState} from "react";
+import { useRouter } from "next/navigation";
+import { saveBookmark } from "@/lib/bookmarkAdd";
 
-export default page;
+const AddBookmarkPage = () => {
+    const [title, setTitle] = useState("");
+    const [url, setUrl] = useState("");
+    const [message, setMessage] = useState<string|null>(null);
+    
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        if(!url) {
+            alert("Please enter URL");
+            return;
+        }
+
+        if(!title) {
+          alert("Please enter Title");
+          return;
+	      }
+
+        const payload = {
+            title, url
+        }
+
+	    try {
+	      const response = await saveBookmark(payload);
+	      console.log("SaveBookmark response: ", response);
+	      setMessage("Bookmark saved successfully");  
+	      await router.push(`/bookmarks`);
+	      router.refresh();
+	    } catch (error) {
+	      console.log('등록 실패!!!!')  
+	      console.log(error);
+	      setMessage("Bookmark saved fail");  
+	    }
+	
+	    setTitle("");
+	    setUrl("");
+	        
+     } //handleSubmit 
+
+    return (
+	  )
+}
+
+export default AddBookmarkPage;
