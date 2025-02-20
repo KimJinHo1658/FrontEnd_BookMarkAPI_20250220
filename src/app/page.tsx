56a52d7be5bce234dbe11545019a777b96e59165
psz3@wavus.co.kr <psz3@wavus.co.kr>
2025-02-20 16:09:58 +0900

    Bookmarks 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { getAllBookmarks } from "@/lib/bookmarkApi";
+import { BookmarkType, BookmarksResponse } from "@/types";
+import Bookmarks from '@/app/components/Bookmarks';
 
-export default function Home() {
+type Props = {
+  searchParams: { page: string }
+}
+
+export default async function BookmarkPage(props: Props)  {
+  const { page = 1 } = props.searchParams
+  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks(parseInt(String(page)))
+  const { data } = await bookmarksData
+  const bookmarks: BookmarkType[] = data
+
   return (
     <div>
-        <h1>Welcome to Bookmarker</h1>
+        <Bookmarks bookmarks={bookmarks} />
     </div>
   );
 }
