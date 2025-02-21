06d585f0cc5f8d3c854d9bc73540216154fa7fe3
psz3@wavus.co.kr <psz3@wavus.co.kr>
2025-02-21 10:29:25 +0900

    client docker build push

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -3,12 +3,12 @@ import { BookmarksResponse } from "@/types";
 import Bookmarks from '@/app/components/Bookmarks';
 import SearchForm from "@/app/components/SearchForm";
 
-type Props = {
-  searchParams: { page: string, query: string }
+type Props ={
+  searchParams: Promise<{ page: string, query?: string | undefined }>
 }
 
-export default async function BookmarkPage(props: Props)  {
-  const { page = 1, query = "" } = props.searchParams
+export default async function BookmarkPage(props:Props)  {
+  const { page = 1, query = "" } = await props.searchParams
   const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks(parseInt(String(page)), query)
   const bookmarkRes = await bookmarksData
 
