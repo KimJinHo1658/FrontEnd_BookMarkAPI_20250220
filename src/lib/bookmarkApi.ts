655abfeb1f95b9729fe6847f8a62b8293f9b9d7d
psz3@wavus.co.kr <psz3@wavus.co.kr>
2025-02-21 09:19:45 +0900

    hhh

--- src/lib/bookmarkApi.ts
+++ src/lib/bookmarkApi.ts
@@ -1,11 +1,14 @@
 import { BookmarksResponse } from "@/types"
-import axios, { AxiosResponse } from "axios"
+import axios from "axios"
 
-const API_BASE_URL = 'http://localhost:8080'
+const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER_API_BASE_URL
 
-export async function getAllBookmarks(page: number): Promise<BookmarksResponse> {
+export async function getAllBookmarks(page: number, query: string): Promise<BookmarksResponse> {
     
-    const API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
+    let API_URL = `${API_BASE_URL}/api/bookmarks?page=${page}`
+    if(query) {
+        API_URL += `&query=${query}`
+    }
     console.log(`url = ${API_URL}`)
     
     const res = await axios.get<BookmarksResponse>(API_URL)
@@ -14,6 +17,6 @@ export async function getAllBookmarks(page: number): Promise<BookmarksResponse> 
 }
 
 export async function saveBookmark(bookmark:{title: string, url: string}) {
-    const res: AxiosResponse<unknown, unknown> = await axios.post(`${API_BASE_URL}/api/bookmarks`, bookmark)
+    const res = await axios.post(`/api/bookmarks`, bookmark)
     return res.data
 }
